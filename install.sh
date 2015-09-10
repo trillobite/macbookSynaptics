a9fab5bbc8be9834edc3ae79c292b79895429105
trillobite <jparnell0@gmail.com>
2015-09-10 08:18:42 -0700

    updated install script

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ make
 sudo rmmod appletouch 
 sudo insmod appletouch.ko 
 sudo cp appletouch.ko /lib/modules/$(uname -r)/kernel/drivers/input/mouse/appletouch.ko
+sudo cp 10-synaptics.conf /usr/share/X11/xorg.conf.d
 
 echo "All Done."
